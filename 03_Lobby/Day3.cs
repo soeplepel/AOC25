719c664acfc1f2d8c2f905e8f7a233c598684bc7
Dries <dries_leeman@hotmail.com>
2025-12-03 19:49:43 +0100

    start day 3

---
+++ 03_Lobby/Day3.cs
@@ -0,0 +1,57 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace _03_Lobby
+{
+    public class Day3
+    {
+        private readonly string[] _lines;
+        public Day3(string input)
+        {
+            _lines = input.Split("\r\n", StringSplitOptions.RemoveEmptyEntries);
+        }
+
+        private Tuple<int, int> GetLargstDigit(char[] arr, int index = 0)
+        {
+            var currentLargest = 0;
+            var indexLargest = 0;
+            for (; index < arr.Length; index++)
+            {
+                var strAtIndex = arr.ElementAt(index) - '0';
+
+                var digit = Convert.ToUInt16(strAtIndex);
+                if (digit > currentLargest)
+                {
+                    currentLargest = digit;
+                    indexLargest = index + 1; ;
+                }
+                   
+
+            }
+
+            if (indexLargest == arr.Length)
+            {
+                arr[indexLargest-1] = '0';
+                var ret =  GetLargstDigit(arr);
+                arr[indexLargest - 1] = Convert.ToChar(currentLargest);
+
+                return ret;
+            }
+                 
+            return new Tuple<int, int>(currentLargest, indexLargest);
+        }
+
+        public int Calculate()
+        {
+            var sum = 0;
+            foreach (var line in _lines)
+            {
+                var charArr = line.ToCharArray();
+                var firstPass = GetLargstDigit(charArr);
+                var secondPass = GetLargstDigit(charArr, firstPass.Item2);
+                sum += Convert.ToUInt16((firstPass.Item1.ToString() + secondPass.Item1.ToString()));
+            }
+
+            return sum;
+        }
+    }
+}
---
+++ PuzzleInputChecker/UnitTest3.cs
@@ -0,0 +1,30 @@
+﻿using _02_GiftShop;
+using _03_Lobby;
+using FluentAssertions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PuzzleInputChecker
+{
+    public class UnitTest3
+    {
+        [Theory]
+        [InlineData("..\\..\\..\\..\\..\\TestData\\Sample_3.txt", 357)]
+       // [InlineData("..\\..\\..\\..\\..\\TestData\\input_3.txt", )]
+        public void GiftShop(string path, int result)
+        {
+            //aarrange
+            var str = File.ReadAllText(path);
+
+            //act
+            Day3 day = new Day3(str);
+            var exampleResult = day.Calculate();
+
+            //assert
+            exampleResult.Should().Be(result);
+        }
+    }
+}
